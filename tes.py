e1493e159d47e125fd3e2c6dab36fbb36bbfed83
agatha-chrisantya <70108492+agatha-chrisantya@users.noreply.github.com>
2023-05-25 20:20:14 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Hello bitj")
+st.set_page_config(
+    page title = "Super Store Dashboard",
+    layout = "wide"
+)
+
+df = pd.read_csv("superstore.csv")
+df["order_date"] = pd.to_datetime(df["order_date"])
+df["ship_date"] = pd.to_datetime(df["ship_date"])
+
+st.title("Superstore Dashboard")
+st.dataframe(df)
 
