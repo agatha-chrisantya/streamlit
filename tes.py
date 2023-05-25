d973801b48191829e7f89005d9306c66c97cc5f9
agatha-chrisantya <70108492+agatha-chrisantya@users.noreply.github.com>
2023-05-25 20:49:00 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -13,6 +13,10 @@ df["ship_date"] = pd.to_datetime(df["ship_date"])
 st.title("Superstore Dashboard")
 st.dataframe(df)
 
+df["order_year"] = df["order_date"].dt.year
+CURR_YEAR = df["order_year"].max()
+PREV_YEAR = CURR_YEAR - 1
+
 with mx_sales:
     curr_sales = mx_data.loc[mx_data["order_year"] == CURR_YEAR, "sales"].values[0]
     prev_sales = mx_data.loc[mx_data["order_year"] == PREV_YEAR, "sales"].values[0]
