3df6d06ff53ae75b6f9984306c9dce631c41d441
agatha-chrisantya <70108492+agatha-chrisantya@users.noreply.github.com>
2023-05-25 20:41:16 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -13,3 +13,12 @@ df["ship_date"] = pd.to_datetime(df["ship_date"])
 st.title("Superstore Dashboard")
 st.dataframe(df)
 
+with mx_sales:
+    curr_sales = mx_data.loc[mx_data["order_year"] == CURR_YEAR, "sales"].values[0]
+    prev_sales = mx_data.loc[mx_data["order_year"] == PREV_YEAR, "sales"].values[0]
+    sales_diff_pct = 100 * (curr_sales - prev_sales) / prev_sales
+    st.metric(
+        label = "Sales",
+        value = curr_sales,
+        delta = f"{sales_diff_pct:.2f}"
+
